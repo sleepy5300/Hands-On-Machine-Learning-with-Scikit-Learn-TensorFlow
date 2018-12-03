a9e21c161e0f8d95e0e283cffcccf24d0b1cfa6c
Mitch Lee <tmac790601@hotmail.com>
2018-12-03 17:24:31 +0800

    Add files via upload

---
+++ Chapter08.py
@@ -0,0 +1,356 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import proj3d
+from matplotlib import rcParams
+
+# Fix minus sign problem
+rcParams['axes.unicode_minus'] = False
+
+np.random.seed(42)
+#%% Projection Methods
+np.random.seed(4)
+m = 60
+w1, w2 = 0.1, 0.3
+noise = 0.1
+
+angles = np.random.rand(m) * 3 * np.pi / 2 - 0.5
+X = np.empty((m ,3))
+X[:, 0] = np.cos(angles) + np.sin(angles) / 2 + noise * np.random.randn(m) / 2
+X[:, 1] = np.sin(angles) * 0.7 + noise * np.random.randn(m) / 2
+X[:, 2] = X[:, 0] * w1 + X[:, 1] * w2 + noise * np.random.randn(m)
+
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(X[:, 0], X[:, 1], X[:, 2], 'b.')
+
+#%% PCA using SVD decomposition
+X_centered = X - X.mean(axis=0)
+U, s, Vt = np.linalg.svd(X_centered)
+c1 = Vt.T[:, 0]
+c2 = Vt.T[:, 1]
+
+m, n = X.shape
+S = np.zeros(X_centered.shape)
+S[:n, :n] = np.diag(s)
+
+#%%
+np.allclose(X_centered, U.dot(S).dot(Vt))
+
+#%%
+W2 = Vt.T[:, : 2]
+X2D = X_centered.dot(W2)
+X2D_using_svd = X2D
+
+#%% PCA using Scikit-Learn
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+X2D = pca.fit_transform(X)
+
+#%%
+np.allclose(X2D, -X2D_using_svd)
+
+#%%
+X3D_inv = pca.inverse_transform(X2D)
+
+#%%
+np.allclose(X3D_inv, X)
+
+#%%
+np.mean(np.sum(np.square(X3D_inv - X), axis=1))
+
+#%%
+X3D_inv_using_svd = X2D_using_svd.dot(Vt[: 2, :])
+np.allclose(X3D_inv_using_svd, X3D_inv - pca.mean_)
+
+#%% Manifold learning
+from sklearn.datasets import make_swiss_roll
+X, t = make_swiss_roll(n_samples=1000, noise=0.2, random_state=42)
+
+axes = [-11.5, 14, -2, 23, -12, 15]
+fig = plt.figure(figsize=(6, 5))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=t, cmap=plt.cm.hot)
+ax.view_init(10, -70)
+ax.set_xlabel("$x_1$", fontsize=18)
+ax.set_ylabel("$x_2$", fontsize=18)
+ax.set_zlabel("$x_3$", fontsize=18)
+ax.set_xlim(axes[0:2])
+ax.set_ylim(axes[2:4])
+ax.set_zlim(axes[4:6])
+
+#%%
+plt.figure(figsize=(11, 4))
+
+plt.subplot(121)
+plt.scatter(X[:, 0], X[:, 1], c=t, cmap=plt.cm.hot)
+plt.axis(axes[:4])
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$x_2$", fontsize=18, rotation=0)
+plt.grid(True)
+
+plt.subplot(122)
+plt.scatter(t, X[:, 1], c=t, cmap=plt.cm.hot)
+plt.axis([4, 15, axes[2], axes[3]])
+plt.xlabel("$z_1$", fontsize=18)
+plt.grid(True)
+
+#%%
+axes = [-11.5, 14, -2, 23, -12, 15]
+
+x2s = np.linspace(axes[2], axes[3], 10)
+x3s = np.linspace(axes[4], axes[5], 10)
+x2, x3 = np.meshgrid(x2s, x3s)
+
+fig = plt.figure(figsize=(6, 5))
+ax = plt.subplot(111, projection='3d')
+positive_class = X[:, 0] > 5
+X_pos = X[positive_class]
+X_neg = X[~positive_class]
+ax.view_init(10, -70)
+ax.plot(X_neg[:, 0], X_neg[:, 1], X_neg[:, 2], "y^")
+ax.plot_wireframe(5, x2, x3, alpha=0.5)
+ax.plot(X_pos[:, 0], X_pos[:, 1], X_pos[:, 2], "gs")
+ax.set_xlabel("$x_1$", fontsize=18)
+ax.set_ylabel("$x_2$", fontsize=18)
+ax.set_zlabel("$x_3$", fontsize=18)
+ax.set_xlim(axes[0:2])
+ax.set_ylim(axes[2:4])
+ax.set_zlim(axes[4:6])
+
+fig = plt.figure(figsize=(5, 4))
+ax = plt.subplot(111)
+plt.plot(t[positive_class], X[positive_class, 1], "gs")
+plt.plot(t[~positive_class], X[~positive_class, 1], "y^")
+plt.axis([4, 15, axes[2], axes[3]])
+plt.xlabel("$z_1$", fontsize=18)
+plt.ylabel("$z_2$", fontsize=18, rotation=0)
+plt.grid(True)
+
+fig = plt.figure(figsize=(6, 5))
+ax = plt.subplot(111, projection='3d')
+positive_class = 2 * (t[:] - 4) > X[:, 1]
+X_pos = X[positive_class]
+X_neg = X[~positive_class]
+ax.view_init(10, -70)
+ax.plot(X_neg[:, 0], X_neg[:, 1], X_neg[:, 2], "y^")
+ax.plot(X_pos[:, 0], X_pos[:, 1], X_pos[:, 2], "gs")
+ax.set_xlabel("$x_1$", fontsize=18)
+ax.set_ylabel("$x_2$", fontsize=18)
+ax.set_zlabel("$x_3$", fontsize=18)
+ax.set_xlim(axes[0:2])
+ax.set_ylim(axes[2:4])
+ax.set_zlim(axes[4:6])
+
+fig = plt.figure(figsize=(5, 4))
+ax = plt.subplot(111)
+plt.plot(t[positive_class], X[positive_class, 1], "gs")
+plt.plot(t[~positive_class], X[~positive_class, 1], "y^")
+plt.plot([4, 15], [0, 22], "b-", linewidth=2)
+plt.axis([4, 15, axes[2], axes[3]])
+plt.xlabel("$z_1$", fontsize=18)
+plt.ylabel("$z_2$", fontsize=18, rotation=0)
+plt.grid(True)
+
+#%% PCA
+angle = np.pi / 5
+stretch = 5
+m = 200
+
+np.random.seed(3)
+X = np.random.randn(m, 2) / 10
+X = X.dot(np.array([[stretch, 0], [0, 1]]))
+X = X.dot([[np.cos(angle), np.sin(angle)], [-np.sin(angle), -np.cos(angle)]])
+
+u1 = np.array([np.cos(angle), np.sin(angle)])
+u2 = np.array([np.cos(angle - 2 * np.pi / 6), np.sin(angle - 2 * np.pi / 6)])
+u3 = np.array([np.cos(angle - np.pi / 2), np.sin(angle - np.pi / 2)])
+
+X_proj1 = X.dot(u1.reshape(-1, 1))
+X_proj2 = X.dot(u2.reshape(-1, 1))
+X_proj3 = X.dot(u3.reshape(-1, 1))
+
+plt.figure(figsize=(8, 4))
+plt.subplot2grid((3, 2), (0, 0), rowspan=3)
+plt.plot([-1.4, 1.4], [-1.4 * u1[1] / u1[0], 1.4 * u1[1] / u1[0]], "k-", linewidth=1)
+plt.plot([-1.4, 1.4], [-1.4 * u2[1] / u2[0], 1.4 * u2[1] / u2[0]], "k--", linewidth=1)
+plt.plot([-1.4, 1.4], [-1.4 * u3[1] / u3[0], 1.4 * u3[1] / u3[0]], "k:", linewidth=2)
+plt.plot(X[:, 0], X[:, 1], "bo", alpha=0.5)
+plt.axis([-1.4, 1.4, -1.4, 1.4])
+plt.arrow(0, 0, u1[0], u1[1], head_width=0.1, linewidth=5, length_includes_head=True, head_length=0.1, fc='k', ec='k')
+plt.arrow(0, 0, u3[0], u3[1], head_width=0.1, linewidth=5, length_includes_head=True, head_length=0.1, fc='k', ec='k')
+plt.text(u1[0] + 0.1, u1[1] - 0.05, r"$\mathbf{c_1}$", fontsize=22)
+plt.text(u3[0] + 0.1, u3[1], r"$\mathbf{c_2}$", fontsize=22)
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$x_2$", fontsize=18, rotation=0)
+plt.grid(True)
+
+plt.subplot2grid((3,2), (0, 1))
+plt.plot([-2, 2], [0, 0], "k-", linewidth=1)
+plt.plot(X_proj1[:, 0], np.zeros(m), "bo", alpha=0.3)
+plt.gca().get_yaxis().set_ticks([])
+plt.gca().get_xaxis().set_ticklabels([])
+plt.axis([-2, 2, -1, 1])
+plt.grid(True)
+
+plt.subplot2grid((3,2), (1, 1))
+plt.plot([-2, 2], [0, 0], "k--", linewidth=1)
+plt.plot(X_proj2[:, 0], np.zeros(m), "bo", alpha=0.3)
+plt.gca().get_yaxis().set_ticks([])
+plt.gca().get_xaxis().set_ticklabels([])
+plt.axis([-2, 2, -1, 1])
+plt.grid(True)
+
+plt.subplot2grid((3,2), (2, 1))
+plt.plot([-2, 2], [0, 0], "k:", linewidth=2)
+plt.plot(X_proj3[:, 0], np.zeros(m), "bo", alpha=0.3)
+plt.gca().get_yaxis().set_ticks([])
+plt.axis([-2, 2, -1, 1])
+plt.xlabel("$z_1$", fontsize=18)
+plt.grid(True)
+
+#%% MNIST compression
+from sklearn.datasets import fetch_mldata
+from sklearn.model_selection import train_test_split
+
+mnist = fetch_mldata('MNIST original')
+X = mnist["data"]
+Y = mnist["target"]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+
+pca = PCA()
+pca.fit(X_train)
+cumsum = np.cumsum(pca.explained_variance_ratio_)
+d = np.argmax(cumsum >= 0.95) + 1
+
+pca = PCA(n_components=154)
+X_reduced = pca.fit_transform(X_train)
+X_recovered = pca.inverse_transform(X_reduced)
+
+def plot_digits(instances, images_per_row=5, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size, size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = matplotlib.cm.binary, **options)
+    plt.axis("off")
+    
+plt.figure(figsize=(7, 4))
+plt.subplot(121)
+plot_digits(X_train[::2100])
+plt.title("Original", fontsize=16)
+plt.subplot(122)
+plot_digits(X_recovered[::2100])
+plt.title("Compressed", fontsize=16)
+
+#%% Incermental PCA
+from sklearn.decomposition import IncrementalPCA
+
+n_batches = 100
+inc_pca = IncrementalPCA(n_components=154)
+for X_batch in np.array_split(X_train, n_batches):
+    print('.', end=' ')
+    inc_pca.partial_fit(X_batch)
+    
+X_reduced = inc_pca.transform(X_train)
+X_recovered_inc_pca = inc_pca.inverse_transform(X_reduced)
+
+plt.figure(figsize=(7, 4))
+plt.subplot(121)
+plot_digits(X_train[::2100])
+plt.subplot(122)
+plot_digits(X_recovered_inc_pca[::2100])
+plt.tight_layout()
+
+#%% Time complexity
+import time
+
+for n_components in (2, 10, 154):
+    print('n_components =', n_components)
+    regular_pca = PCA(n_components=n_components)
+    inc_pca = IncrementalPCA(n_components=n_components, batch_size=500)
+    rnd_pca = PCA(n_components=n_components, random_state=42, svd_solver='randomized')
+    
+    for pca in (regular_pca, inc_pca, rnd_pca):
+        t1 = time.time()
+        pca.fit(X_train)
+        t2 = time.time()
+        print("    {}: {:.1f} seconds".format(pca.__class__.__name__, t2 - t1))
+        
+#%%
+times_rpca = []
+times_pca = []
+sizes = [1000, 2000, 3000, 4000, 5000, 6000]
+for n_features in sizes:
+    X = np.random.randn(2000, n_features)
+    pca = PCA(n_components = 2, random_state=42, svd_solver="randomized")
+    t1 = time.time()
+    pca.fit(X)
+    t2 = time.time()
+    times_rpca.append(t2 - t1)
+    pca = PCA(n_components = 2)
+    t1 = time.time()
+    pca.fit(X)
+    t2 = time.time()
+    times_pca.append(t2 - t1)
+
+plt.figure()
+plt.plot(sizes, times_rpca, "b-o", label="RPCA")
+plt.plot(sizes, times_pca, "r-s", label="PCA")
+plt.xlabel("n_features")
+plt.ylabel("Training time")
+plt.legend(loc="upper left")
+plt.title("PCA and Randomized PCA time complexity")
+
+#%% Kernel PCA
+from sklearn.decomposition import KernelPCA
+
+X, t = make_swiss_roll(n_samples=1000, noise=0.2, random_state=42)
+rbf_pca = KernelPCA(n_components=2, kernel='rbf', gamma=0.04)
+X_reduced = rbf_pca.fit_transform(X)
+
+lin_pca = KernelPCA(n_components=2, kernel='linear', fit_inverse_transform=True)
+rbf_pca = KernelPCA(n_components = 2, kernel="rbf", gamma=0.0433, fit_inverse_transform=True)
+sig_pca = KernelPCA(n_components = 2, kernel="sigmoid", gamma=0.001, coef0=1, fit_inverse_transform=True)
+
+y = t > 6.9
+
+plt.figure(figsize=(11, 4))
+for subplot, pca, title in ((131, lin_pca, "Linear kernel"), (132, rbf_pca, "RBF kernel, $\gamma=0.04$"), (133, sig_pca, "Sigmoid kernel, $\gamma=10^{-3}, r=1$")):
+    X_reduced = pca.fit_transform(X)
+    if subplot == 132:
+        X_reduced_rbf = X_reduced
+    
+    plt.subplot(subplot)
+    plt.title(title, fontsize=14)
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=t, cmap=plt.cm.hot)
+    plt.xlabel("$z_1$", fontsize=18)
+    if subplot == 131:
+        plt.ylabel("$z_2$", fontsize=18, rotation=0)
+    plt.grid(True)
+    
+#%% LLE
+from sklearn.manifold import LocallyLinearEmbedding
+from sklearn.datasets import make_swiss_roll
+
+X, t = make_swiss_roll(n_samples=1000, noise=0.2, random_state=41)
+lle = LocallyLinearEmbedding(n_components=2, n_neighbors=10, random_state=42)
+X_reduced = lle.fit_transform(X)
+
+plt.figure()
+plt.title("Unrolled swiss roll using LLE", fontsize=14)
+plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=t, cmap=plt.cm.hot)
+plt.xlabel("$z_1$", fontsize=18)
+plt.ylabel("$z_2$", fontsize=18)
+plt.axis([-0.065, 0.055, -0.1, 0.12])
+plt.grid(True)
